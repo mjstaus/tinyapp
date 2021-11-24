7781040b33e561d303ecc6e7d17aebf44f10ce38
Martha Staus <mjstaus@gmail.com>
2021-11-24 12:19:10 -0800

    Add route for /urls

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//ROUTES//
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -19,6 +20,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
